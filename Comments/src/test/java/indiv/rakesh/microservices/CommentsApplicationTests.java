ed2a47af74bad9fd715db9e4be35a1b90eaabbd8
Rakesh Malireddy <rakesh.malireddy@gmail.com>
2017-09-07 20:40:57 -0500

    comments_application

---
+++ Comments/src/test/java/indiv/rakesh/microservices/CommentsApplicationTests.java
@@ -0,0 +1,16 @@
+package indiv.rakesh.microservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommentsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
